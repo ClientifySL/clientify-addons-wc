35adc933362179e182610b7420ba876f29352b90
Emerson Ramirez <ramirezemerson1991@gmail.com>
2025-05-02 18:50:06 -0400

    change name version

--- README.txt
+++ README.txt
@@ -4,7 +4,7 @@ Tags: woocommerce, clientify, integration, CRM, ecommerce
 Requires at least: 4.0
 Tested up to: 6.2.2
 Requires PHP: 7.4.2
-Stable tag: 1.1.0
+Stable tag: 1.1.1
 License: GPL-2.0-or-later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
--- clientify-addons.php
+++ clientify-addons.php
@@ -9,14 +9,14 @@
  * that starts the plugin.
  *
  * @link              Clientify SL
- * @since             1.1.0
+ * @since             1.1.1
  * @package           Clientify-Ecommerce
  *
  * @wordpress-plugin
  * Plugin Name:       Clientify-Ecommerce
  * Plugin URI:        https://clientify.com/
  * Description:       Conecta woocommerce con Clientify para automatizar el marketing de tu tienda online.
- * Version:           1.1.0
+ * Version:           1.1.1
  * Author:            Clientify SL
  * Author URI:        Clientify SL
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'CLIENTIFY_ADDONS_VERSION', '1.1.0' );
+define( 'CLIENTIFY_ADDONS_VERSION', '1.1.1' );
 
 /**
  * The code that runs during plugin activation.
